75e7f9e24549ec33e566cb25aac1c19d8b4c9abd
A_pen <penguinpenguinxx@yahoo.co.jp>
2023-04-21 14:34:26 +0000

    [AC] abc298 abc298_d

---
+++ atcoder.jp/abc298/abc298_d/Main.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+#include <atcoder/modint>
+using namespace atcoder;
+using mint = modint998244353;
+using namespace std;
+int main() {
+	
+	int Q;
+	cin>>Q;
+    
+	deque<int> S(1,1);
+	mint ans = 1;
+	
+	for(int i=0;i<Q;i++){
+		int t;
+		cin>>t;
+		if(t==1){
+			int x;
+			cin>>x;
+			S.push_back(x);
+			ans = ans*10 + x;
+		}
+		if(t==2){
+			ans -= mint(10).pow(S.size()-1) * S.front();
+			S.pop_front();
+		}
+		if(t==3){
+			cout<<ans.val()<<endl;
+		}
+	}
+	
+	return 0;
+}
