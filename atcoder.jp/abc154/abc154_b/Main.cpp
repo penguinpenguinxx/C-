47484caa8f8a432099472fc2672dc206e9fc85c6
A_pen <penguinpenguinxx@yahoo.co.jp>
2023-04-26 14:00:25 +0000

    [AC] abc154 abc154_b

---
+++ atcoder.jp/abc154/abc154_b/Main.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+#include <atcoder/all>
+using namespace atcoder;
+using namespace std;
+# define rep(i, n) for (int i = 0; i < (int)(n); i++)
+int main() {
+    string S;
+    cin >> S;
+    string t(S.size(),'x');
+    cout << t << endl;
+
+
+}
